b593c5bf65bb4811c3824444eccd4f966b7f4e0e
Pham Duc Long <longpdgch17143@fpt.edu.vn>
2019-08-06 14:25:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Hello Heroku</title>
+        <title>test</title>
     </head>
     <body>
-        <?php
-        echo "Hello Heroku";
+    	<?php
+        	//$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=GWCourses', 'postgres', 'phamduclong99');
+        	//echo "fail!!!!!!";
+                
+        $db = parse_url(getenv("DATABASE_URL"));
+
+        $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+));
+        
+                $sql = "SELECT studentname, course FROM registercourse";
+		$stmt = $pdo->prepare($sql);
+		//Thiết lập kiểu dữ liệu trả về
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
         ?>
+        <ul>
+		<?php  
+			foreach ($resultSet as $row) {
+			echo '<li>' .
+				$row['studentname'] . ' --' . $row['course'] 
+				. '</li>';
+			}
+		?>
+	</ul>
     </body>
 </html>
